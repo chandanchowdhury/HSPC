c011f60a8d295a8103e03c1add89c392a9c6892f
i_virus <ivirus.aec@gmail.com>
2017-03-17 12:58:27 -0500

    improved parameter usage

--- run-postgres-in-docker.sh
+++ run-postgres-in-docker.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/bash
 #docker pull postgres
 
 POSTGRES_DB=HSPC
 POSTGRES_USER=hspc
 # TODO: Get the password from command line or environment
-POSTGRES_PASSWORD=password
+POSTGRES_PASSWORD=HSPC-Password
 
 PWD=`pwd`
 
 # where the DB files will be stored
-db_dir=$PWD/postgres_db
+DBPATH=$PWD/postgres_db
 
 # make sure the DB directory exists
 mkdir -p db_dir
@@ -18,6 +19,6 @@ docker run -d -t -p 5432:5432 --rm \
     --name HSPC-postgres \
     -e POSTGRES_DB=$POSTGRES_DB \
     -e POSTGRES_USER=$POSTGRES_USER \
-    -e POSTGRES_PASSWORD=POSTGRES_PASSWORD \
-    -v $PWD/postgres_db:/var/lib/postgresql/data \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -v $DBPATH:/var/lib/postgresql/data \
     postgres
