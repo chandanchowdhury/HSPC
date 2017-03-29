0778306febbc865dd440fdde58ab13f7dd6c4d7c
i_virus <ivirus.aec@gmail.com>
2017-03-29 00:15:04 -0500

    added clean parameter to remove the directories and rebuild from scratch

--- build.sh
+++ build.sh
@@ -5,11 +5,17 @@
 # export GOBIN=$GOPATH/bin
 # export $PATH=$GOBIN:$PATH
 
+
 swagger validate api_spec.yaml
+
 
 if [ $? -eq 0 ]
 then
-    # rm -rf cmd restapi models
+    if [ "$1" = "clean" ]
+    then
+        echo "Deleting direcotries..."
+        rm -rf cmd restapi models
+    fi
     swagger generate server -A hspc -f api_spec.yaml
 
     go get -u -f ./...
