3e3a68f89eede14ac6a62cdd3cc411d6cb2f15ba
i_virus <ivirus.aec@gmail.com>
2017-03-29 00:13:54 -0500

    corrected the sequence of drops to follow dependency

--- drop_tables.sql
+++ drop_tables.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS EmailAddress;
-DROP TABLE IF EXISTS Address;
-DROP TABLE IF EXISTS School;
+/*
+Maintain the order as they follow they dependency.
+*/
+DROP TABLE IF EXISTS StudentTeam;
+DROP TABLE IF EXISTS Team;
 DROP TABLE IF EXISTS Advisor;
 DROP TABLE IF EXISTS Student;
-DROP TABLE IF EXISTS Team;
-DROP TABLE IF EXISTS StudentTeam;
+DROP TABLE IF EXISTS School;
+DROP TABLE IF EXISTS EmailAddress;
+DROP TABLE IF EXISTS Address;
