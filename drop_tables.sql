0cd2e10d8491d5b45d5da08aa5949e94f9caee7b
i_virus <ivirus.aec@gmail.com>
2017-03-31 22:50:06 -0500

    added all the table in required order

--- drop_tables.sql
+++ drop_tables.sql
@@ -1,10 +1,12 @@
 /*
 Maintain the order as they follow they dependency.
 */
-DROP TABLE IF EXISTS StudentTeam;
-DROP TABLE IF EXISTS Team;
-DROP TABLE IF EXISTS Advisor;
-DROP TABLE IF EXISTS Student;
-DROP TABLE IF EXISTS School;
-DROP TABLE IF EXISTS EmailAddress;
-DROP TABLE IF EXISTS Address;
+DROP TABLE IF EXISTS parking;
+DROP TABLE IF EXISTS team_score;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS team;
+DROP TABLE IF EXISTS school_advisor;
+DROP TABLE IF EXISTS advisor;
+DROP TABLE IF EXISTS school;
+DROP TABLE IF EXISTS credential;
+DROP TABLE IF EXISTS address;
