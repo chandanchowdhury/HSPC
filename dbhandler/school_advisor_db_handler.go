0ead2202de95dd7b2103974902a6f83955ded520
i_virus <ivirus.aec@gmail.com>
2017-05-09 01:27:13 -0500

    functions to maintain School Advisor relation

---
+++ dbhandler/school_advisor_db_handler.go
@@ -0,0 +1,105 @@
+package dbhandler
+
+import "log"
+
+/**
+  SchoolAddAdvisor adds an Advisor to a School
+*/
+func SchoolAddAdvisor(school_id int64, advisor_id int64) bool {
+	log.Print("# Add School Advisor")
+	log.Printf("School ID = %d, Advisor ID = %d", school_id, advisor_id)
+
+	db := getDBConn()
+	stmt, err := db.Prepare("INSERT INTO School_Advisor(school_id, advisor_id) " +
+		"VALUES($1, $2)")
+	defer stmt.Close()
+
+	if err != nil {
+		log.Print("Error creating prepared statement")
+		log.Panic(err)
+	}
+
+	result, err := stmt.Exec(school_id, advisor_id)
+
+	if err != nil {
+		if isForeignKeyError(err) {
+			return false
+		}
+
+		log.Panic(err)
+	}
+
+	affectedRows, err := result.RowsAffected()
+	if affectedRows != 1 {
+		log.Panic("Unexpected number of inserts")
+	}
+
+	return true
+}
+
+/**
+SchoolDeleteAdvisor deletes an Advisor from a School
+*/
+func SchoolDeleteAdvisor(school_id int64, advisor_id int64) bool {
+	log.Print("# Remove School Advisor")
+	log.Printf("School ID = %d, Asvisor ID = %d", school_id, advisor_id)
+
+	db := getDBConn()
+
+	delete_stmt, err := db.Prepare("DELETE FROM School_Advisor WHERE school_id = $1 AND advisor_id = $2")
+	defer delete_stmt.Close()
+
+	if err != nil {
+		log.Print("Error creating prepared statement")
+		log.Panic(err)
+	}
+
+	result, err := delete_stmt.Exec(school_id, advisor_id)
+
+	affectedRows, err := result.RowsAffected()
+	if affectedRows != 1 {
+		log.Panic("Unexpected number of deletion")
+	}
+
+	if err != nil {
+		if isForeignKeyError(err) {
+			return false
+		}
+
+		log.Panic(err)
+	}
+
+	return true
+}
+
+func SchoolReadAdvisors(school_id int64) []int64 {
+	log.Print("# Reading School Advisors")
+	log.Printf("School ID = %d", school_id)
+
+	db := getDBConn()
+
+	stmt, err := db.Prepare("SELECT advisor_id " +
+		"FROM School_Advisor WHERE school_id = $1")
+	defer stmt.Close()
+
+	if err != nil {
+		log.Print("Error creating prepared statement")
+		log.Panic(err)
+	}
+
+	crsr, err := stmt.Query(school_id)
+
+	if err != nil {
+		log.Print("Error getting team data")
+		log.Panic(err)
+	}
+
+	advisor_ids := make([]int64, 0)
+	var a_id int64
+	for crsr.Next() {
+		crsr.Scan(&a_id)
+		advisor_ids = append(advisor_ids, a_id)
+	}
+
+	return advisor_ids
+}
