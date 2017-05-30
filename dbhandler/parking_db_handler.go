c657905b414a2c9b7f73a0f3f17f29e2a4e40b93
i_virus <ivirus.aec@gmail.com>
2017-05-30 18:06:02 -0500

    dummy dbhandler functions

---
+++ dbhandler/parking_db_handler.go
@@ -0,0 +1,32 @@
+package dbhandler
+
+//Parking
+
+/*
+func parkingCreate(parking models.Parking) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
+
+func parkingRead(parking_id int64) models.Parking {
+	var parking = models.Parking{}
+
+	//TODO: complete the logic
+
+	return parking
+}
+
+func parkingUpdate(parking models.Parking) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
+
+func parkingDelete(parking_id int64) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
+
+*/
---
+++ dbhandler/team_score_db_handler.go
@@ -0,0 +1,31 @@
+package dbhandler
+
+import "github.com/chandanchowdhury/HSPC/models"
+
+//Team Score
+
+func teamscoreCreate(teamscore models.TeamScore) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
+
+func teamscoreRead(teamscore_id int64) models.TeamScore {
+	var teamscore = models.TeamScore{}
+
+	//TODO: complete the logic
+
+	return teamscore
+}
+
+func teamscoreUpdate(teamscore models.TeamScore) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
+
+func teamscoreDelete(teamscore_id int64) int64 {
+	//TODO: complete the logic
+
+	return 0
+}
